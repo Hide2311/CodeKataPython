073576f40ee991166642c6dc87b55257fa859367
Mehi <mehi2311@gmail.com>
2013-08-08 11:51:47 +0900

    #2 fix

--- Exercises/src/01_hello.py
+++ Exercises/src/01_hello.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
+
 import sys
 
-count = int(sys.argv[1])
+if len(sys.argv) != 2:
+    print u"引数を入力してください。"
+    quit()
 
+count = int(sys.argv[1])
 for i in range(0, count):
-    print "Hello World!", i+1
+    print "Hello World!", i + 1
+
+
